27e083f6c2eb6fbeb10a32b01b55f0574c2f7581
sagharnoursobhi <saghar.noursobhi@hyperisland.se>
2023-01-10 12:30:56 +0100

    add annimation to header button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,38 @@
 import classes from './headerCartBtn.module.css';
 import CartIcon from '../Cart/CartIcon';
-import { useContext } from 'react';
-import CartContext from '../../store/CartContext'
+import { useContext, useState, useEffect } from 'react';
+import CartContext from '../../store/CartContext';
 
 export default function HeaderCartButton({ showCard }) {
 
     const cartCtx = useContext(CartContext);
+    const { items } = cartCtx;
     
-    const totalAmountOfItems = cartCtx.items.reduce((currNum, item) => {
+    const totalAmountOfItems = items.reduce((currNum, item) => {
         return currNum + item.amount
     }, 0);
 
+
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+    const buttonClasses = `${btnIsHighlighted ? classes.bump : ''} ${classes.button}`;
+
+    useEffect(()=>{
+        if(items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);//in order to have animation permanently
+        const timer = setTimeout(()=>{
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return ()=>{
+            clearTimeout(timer);
+        }
+
+    }, [items]);
+
     return(
-        <button className={classes.button} onClick={showCard}>
+        <button className={buttonClasses} onClick={showCard}>
             <span className={classes.icon}><CartIcon /></span>
             <span>Your Cart</span>
             <span className={classes.badge}>{ totalAmountOfItems }</span>
